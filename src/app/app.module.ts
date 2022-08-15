14d3f00e094df8150f0d2f38069ec5c54de1d2de
Rodrigo Adriano Meyer <rodrigo.meyer@totvs.com.br>
2022-08-14 21:50:23 -0300

    feat(app): adiciona APP_BASE_HREF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -20,6 +21,7 @@ import { ComponentsModule } from './libs/component/components.module';
     NavbarModule
   ],
   declarations: [AppComponent],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
